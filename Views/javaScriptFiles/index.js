4c1abbd641f101568ad3f293967ad0e6dc768b0c
newsongbammy <newsongbammy@gmail.com>
2024-08-20 16:35:04 +0100

    Worked on the login and forgotten password page and form validation, added the back to top arrow

--- Views/javaScriptFiles/index.js
+++ Views/javaScriptFiles/index.js
@@ -1,6 +1,6 @@
-window.addEventListener('scroll', function() {
+window.addEventListener('scroll', function () {
     const navbar = document.getElementById('navbar');
-    
+
     if (window.scrollY > 1) { // Change this value to adjust when the background changes
         navbar.classList.add('scrolled');
         navbar.classList.remove('unscrolled');
@@ -8,4 +8,25 @@ window.addEventListener('scroll', function() {
         navbar.classList.add('unscrolled');
         navbar.classList.remove('scrolled');
     }
+});
+
+
+const backToTop = document.getElementById('back-to-top');
+
+window.addEventListener('scroll', () => {
+    if (window.scrollY > 700) { // adjust the scroll position value as needed
+        backToTop.classList.add('show');
+    } else {
+        backToTop.classList.remove('show');
+    }
+});
+
+if (window.scrollY < document.body.scrollHeight / 2) { // adjust the scroll position value as needed
+    backToTop.classList.add('up');
+} else {
+    backToTop.classList.remove('up');
+}
+
+backToTop.addEventListener('click', () => {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
 });
---
+++ Views/javaScriptFiles/login.js
@@ -0,0 +1,31 @@
+let emailInput = document.getElementById("email");
+let passwordInput = document.getElementById("password");
+
+function loginValidation() {
+
+    email = emailInput.value;
+    password = passwordInput.value;
+    const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+
+    if (email === "" && password === "") {
+        document.getElementById("error-message").style.display = "block";
+        document.getElementById("error-message").innerHTML = "No field can be empty";
+    } else if (email !== "" && password === "") {
+        document.getElementById("error-message").style.display = "block";
+        document.getElementById("error-message").innerHTML = "Please enter a password";
+    } else if (password !== "" && email === "") {
+        document.getElementById("error-message").style.display = "block";
+        document.getElementById("error-message").innerHTML = "Please enter an Email";
+    } else if (!emailRegex.test(email)) {
+        document.getElementById("error-message").style.display = "block";
+        document.getElementById("error-message").innerHTML = "Invalid email address";
+    } else if (password.length < 8) {
+        document.getElementById("error-message").style.cssText = "display: block; padding: 5px 12px 2px 12px";
+        document.getElementById("error-message").innerHTML = "Password must be at least 8 characters";
+    } else {
+        document.getElementById("error-message").style.display = "block";
+        document.getElementById("error-message").innerHTML = "Login Successful"
+        console.log('Login Successful');
+
+    }
+}
