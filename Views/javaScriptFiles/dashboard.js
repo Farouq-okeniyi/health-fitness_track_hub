07262e3f0765552758bb02a9d54250dc4149b2d6
newsongbammy <newsongbammy@gmail.com>
2024-08-28 09:53:32 +0100

    Made some changes to the main dashboard

--- Views/javaScriptFiles/dashboard.js
+++ Views/javaScriptFiles/dashboard.js
@@ -4,6 +4,7 @@ let searchBtn = document.querySelector(".search");
 const closeButton = document.querySelector("#close-btn");
 const arrow = document.querySelector(".TOGGLE")
 const rowFour = document.querySelector("#row-four")
+sectionTwo = document.querySelector(".sub-sec")
 
 toggleBtn.onclick = function () {
     sidebar.classList.toggle("active");
@@ -23,9 +24,22 @@ arrow.addEventListener('click', () => {
 })
 
 function toggleRowFour() {
-    const rowFour = document.getElementById('row-four');
-    const sectionTwo = document.querySelector('.section-two');
 
     rowFour.classList.toggle('collapsed');
     sectionTwo.classList.toggle('fill-gap');
 }
+
+document.addEventListener('DOMContentLoaded', function () {
+    // Get the current page filename
+    var path = window.location.pathname;
+    var page = path.split("/").pop();
+
+    // Remove .html from the filename if it exists
+    page = page.replace('.html', '');
+
+    // Find the link that corresponds to the current page and add the 'active' class
+    var link = document.getElementById(page + '-link');
+    if (link) {
+        link.classList.add('active');
+    }
+});
