f3ab061c01c9f7792fb772745ea6a1e115d52a72
newsongbammy <newsongbammy@gmail.com>
2024-09-02 10:35:13 +0100

    Made some adjustments to the landing page and forgotten password page

--- Views/javaScriptFiles/forgotpassword.js
+++ Views/javaScriptFiles/forgotpassword.js
@@ -7,14 +7,14 @@ function formValidation() {
     email = emailInput.value;
 
     if (email === "") {
-        errorMessage.style.cssText = "display: block; padding: 5px 1px;";
+        errorMessage.classList.add("show", "invalid")
         errorMessage.innerHTML = `<img src='${"./../resourcesFiles/Vector (5).png"}' alt='Error Icon'> Cannot be empty`;
     } else if (!emailRegex.test(email)) {
-        errorMessage.style.cssText = "display: block; padding: 5px 12px 2px 12px";
+        errorMessage.classList.add("show", "invalid")
         errorMessage.innerHTML = `<img src='${"./../resourcesFiles/Vector (5).png"}' alt='Error Icon'> Invalid Email Address`;
     } else {
-        document.getElementById('sub-container').style.display = "none"
-        document.getElementById("message").style.display = "block";
+        document.getElementById('sub-container').style.cssText = "opacity: 0";
+        document.getElementById("message").style.cssText = "opacity: 1; display: block;";
         console.log('Email Reset Successful');
     }
 }
