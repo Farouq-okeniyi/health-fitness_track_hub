146b6c2b2da8888b33d61a4ecbdbe4682a478683
DESKTOP-1M8BI2T\USER <farouqokeniyi2004@gmail.com>
2024-09-03 13:27:26 +0100

    comitt changes

--- Model/authModel.js
+++ Model/authModel.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose')
 const bcrypt = require('bcrypt');
+const { date } = require('joi');
 
 //schema for sighnup
 // Schema for signup
@@ -14,9 +15,13 @@ const userSchema = new mongoose.Schema({
 
     email:          {type: String,required: [true, 'Please enter your email'], unique:[true, 'Email already in use']},
 
+    role:           {type:String,enum:['user',,'doctor']},
+
     password:       {type: String,required: [true, 'Please enter a password'], select:false},
 
-    confirmPassword:{type: String,required: [true, 'Please confirm your password']}
+    confirmPassword:{type: String,required: [true, 'Please confirm your password']},
+
+    changedPasswordAT:Date
 });
  userSchema.pre('save', async function(next){
     if(!this.isModified('password')) return next();
@@ -31,6 +36,19 @@ const userSchema = new mongoose.Schema({
  userSchema.methods.comparePasswordInDB = async function(pswd,pswddb) {
    return await bcrypt.compare(pswd,pswddb)
  }
+
+ userSchema.methods.isChangedPassword =async function(JWTTIMESTAMP) {
+  console.log("changed password????")
+ if (this.changedPasswordAT){
+  const ptwdChangedTimeStamp = parseInt(this.changedPasswordAT.getTime() / 1000,10)
+    console.log(ptwdChangedTimeStamp, JWTTIMESTAMP)
+
+    return   JWTTIMESTAMP < ptwdChangedTimeStamp
+
+ }
+ return false
+ }
+
 const User = mongoose.model(('createdusers'), userSchema);
 
 module.exports = User;
